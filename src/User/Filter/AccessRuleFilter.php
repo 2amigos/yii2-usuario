77430fbb74ff4fa91e99e68c81499f017f1d6690
Antonio Ramirez <amigo.cobos@gmail.com>
2017-11-12 21:46:06 +0100

    re #108 $roleParams was not defined in scope

--- src/User/Filter/AccessRuleFilter.php
+++ src/User/Filter/AccessRuleFilter.php
@@ -43,9 +43,11 @@ class AccessRuleFilter extends AccessRule
                     return true;
                 }
             } else {
-                if (!isset($roleParams)) {
-                    $roleParams = $this->roleParams instanceof Closure ? call_user_func($this->roleParams, $this) : $this->roleParams;
-                }
+
+                $roleParams = $this->roleParams instanceof Closure
+                    ? call_user_func($this->roleParams, $this)
+                    : $this->roleParams;
+
                 if ($user->can($role, $roleParams)) {
                     return true;
                 }
