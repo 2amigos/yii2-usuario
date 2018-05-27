f2605c77df694765c5c0823f0865ea9064147b3a
Administrator <admin@example.com>
2018-05-27 02:00:15 +0200

    Added content

--- src/User/resources/views/settings/privacy.php
+++ src/User/resources/views/settings/privacy.php
@@ -1,5 +1,9 @@
 <?php
+
 use yii\helpers\Html;
+
+$this->title = Yii::t('usuario', 'Privacy settings');
+
 ?>
 
 <div class="row">
@@ -9,10 +13,35 @@ use yii\helpers\Html;
     <div class="col-md-9">
         <div class="panel panel-default">
             <div class="panel-heading">
-                <?= Html::encode($this->title) ?>
+                <h3 class="panel-title"><?= Html::encode($this->title) ?></h3>
             </div>
             <div class="panel-body">
-
+                <div class="row">
+                    <div class="col-md-6">
+                        <h3><?= Yii::t('usuario', 'Export my data') ?></h3>
+                        <p><?= Yii::t(
+                                'usuario',
+                                'Here you can download your personal data in a comma separated values format.'
+                            ) ?>
+                        </p>
+                        <?= Html::a(Yii::t('usuario', 'Download my data'),
+                            ['/user/settings/export'],
+                            ['class' => 'btn btn-info'])
+                        ?>
+                    </div>
+                    <div class="col-md-6">
+                        <h3><?= Yii::t('usuario', 'Delete my account') ?></h3>
+                        <p><?= Yii::t(
+                                'usuario',
+                                'This will remove your personal data from this site. You will no longer be able to sign in.'
+                            ) ?>
+                        </p>
+                        <?= Html::a(Yii::t('usuario', 'Delete'),
+                            ['/user/settings/delete'],
+                            ['class' => 'btn btn-danger'])
+                        ?>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
