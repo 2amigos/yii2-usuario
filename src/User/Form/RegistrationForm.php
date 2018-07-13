4c70824ddcfaa4e0d0be01bf2e0301c231c09c50
Lorenzo Milesi <maxxer@yetopen.it>
2018-07-13 09:39:25 +0200

    Fix typo

--- src/User/Form/RegistrationForm.php
+++ src/User/Form/RegistrationForm.php
@@ -103,7 +103,7 @@ class RegistrationForm extends Model
     public function attributeHints()
     {
         return [
-            'gdpr_consent' => Yii::t('usuario', 'I aggree processing of my personal data and the use of cookies to facilitate the operation of this site. For more information read our {privacyPolicy}',
+            'gdpr_consent' => Yii::t('usuario', 'I agree processing of my personal data and the use of cookies to facilitate the operation of this site. For more information read our {privacyPolicy}',
                 [
                     'privacyPolicy' => Html::a(Yii::t('usuario', 'privacy policy'),
                         $this->module->GDPRprivacyPolicyUrl,
